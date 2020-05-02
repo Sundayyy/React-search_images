fa3759d2c82ed8b9c33025ec19f2a49da7b930e4
Sundayss <minhmanitobss09@gmail.com>
2020-05-02 09:26:54 +0700

    Updates

---
+++ precache-manifest.2d8f68a67b3c6f3f158678f645c32438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b004e4bb3d4b86f4c12a7db273417f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "12988f8ff430faf77197",
+    "url": "/react-gh-pages/static/css/main.03cff1ed.chunk.css"
+  },
+  {
+    "revision": "4779f1eec2e5716cd403",
+    "url": "/react-gh-pages/static/js/2.ed6e4003.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.ed6e4003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12988f8ff430faf77197",
+    "url": "/react-gh-pages/static/js/main.ba22300f.chunk.js"
+  },
+  {
+    "revision": "8c198509d9ad60b798c1",
+    "url": "/react-gh-pages/static/js/runtime-main.51541cba.js"
+  }
+]);
